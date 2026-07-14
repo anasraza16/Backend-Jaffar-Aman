06f5204e319fc999addde518d1e82b822be270a4
Ahmed Raza <razaplus360@gmail.com>
2026-07-15 01:09:53 +0500

    first commit

---
+++ Class-01/server.js
@@ -0,0 +1,45 @@
+// console.log("Hello");
+const num1 = 100;
+const num2 = 200;
+console.log("result==>", num1 + num2);
+
+import fs from "fs"
+
+// const createFile = () => {
+    // console.log("fileCreated==>");
+//     fs.writeFileSync("user.txt", "Ahmed Baqai")
+// }
+
+// createFile()
+
+// IIFE (Immediately Invoked Function Expression) ( ) ()
+
+// const CreateFile = (() => {
+//     console.log("fileCreated==>");
+//     fs.writeFileSync("Sarif.txt", "Baqai Sahab")
+// })()
+
+
+// NOTE: Only can be read on terminal
+
+// const readFile = () => {
+//     console.log("fileReaded==>")
+
+//     const read = fs.readFileSync("./user.txt" , "utf-8")
+
+//     console.log(read);
+    
+// }
+// readFile()
+
+// IIFE
+
+// const readFile = (() => {
+//     console.log("fileReaded==>")
+
+//     const read = fs.readFileSync("./sarif.txt", "utf-8")
+
+//     console.log(read);
+    
+// })()
+
